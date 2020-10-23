f4e95c9ce10677857a7a2d456c4f1916320c23ac
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-22 21:39:15 -0500

    ewfewf
    wefwe

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -6,6 +6,6 @@ router.get('/usuarios', adminController.usuarios);
 router.get('/productos', adminController.productos);
 
 router.get('/usuario/:id', adminController.eliminarUsuario);
-router.post('/usuario/:id',)
+router.post('/usuario/:id', adminController.actualizarUsuario);
 
 module.exports = router;
