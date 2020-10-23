bfa5f197fd52fa6b32a5297427089dbc0b77e12d
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-22 19:52:44 -0500

    wefew
    wef

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -5,10 +5,18 @@ exports.vistaAdmin = (req,res) => {
 }
 
 exports.usuarios = async (req,res) => {
-    res.render('admin/usuarios');
+
+    let sql = "SELECT * FROM usuarios";
+    await mysql.query(sql, (err, resultado) => {
+        if(err) {
+            res.status(401).json({ err: err });
+        } else {
+            console.log(resultado);
+            res.render('admin/usuarios');
+        }
+    });
 }
 
 exports.productos = (req,res) => {
     res.render('admin/productos');
 }
-
