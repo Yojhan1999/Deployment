ce75f424d1f20880a5d75b73b0f45aabed891141
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-22 19:47:36 -0500

    wef
    wefew

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -7,7 +7,7 @@ exports.vistaAdmin = (req,res) => {
 exports.usuarios = async (req,res) => {
 
     let sql = "SELECT * FROM usuarios";
-    await mysql.query(sql, newUser, (err, resultado) => {
+    await mysql.query(sql, (err, resultado) => {
         if(err) {
             res.status(401).json({ err: err });
         } else {
