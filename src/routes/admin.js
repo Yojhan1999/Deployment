bffb2508f22bb5a3e094d80dd513644dad2c4a7b
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-22 20:55:59 -0500

    wef
    wef

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,6 +5,6 @@ router.get('/admin', adminController.vistaAdmin);
 router.get('/usuarios', adminController.usuarios);
 router.get('/productos', adminController.productos);
 
-router.delete('/usuario/:id', authController.eliminarUsuario);
+router.delete('/usuario/:id', adminController.eliminarUsuario);
 
 module.exports = router;
