7fb6b201883b6ea3d816d6a6360c0f8c166f2c38
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-23 21:02:34 -0500

    wefew
    wefw

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const usuarioController = require('../controllers/usuarioController');
 
 router.get('/user/:id', usuarioController.vistaUsuario);
-router.get('/pedido/:producto/:usuario')
+router.get('/pedido/:producto/:usuario', usuarioController.realizarPedido);
 
 module.exports = router;
