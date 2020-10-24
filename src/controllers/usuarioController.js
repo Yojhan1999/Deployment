82c5870df43ba404019597e645199f049f140cdc
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-23 20:48:44 -0500

    rrwegr
    wergffwreg

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const mysql = require('../database');
+const moment = require('moment');
 
 exports.vistaUsuario = async (req,res) => {
     let sql = "SELECT * FROM productos";
@@ -8,8 +9,24 @@ exports.vistaUsuario = async (req,res) => {
         } else {
             res.render('usuario/index', {
                 helados: resultado,
-                id: req.params.id
+                id_usuario: req.params.id
             });
+        }
+    });
+}
+
+exports.realizarPedido = async (req,res) => {
+    let newPedido = {
+        id_usuario: req.params.usuario,
+        id_producto: req.params.producto,
+        fecha_pedido: moment(Date.now()).format("YYYY-MM-DD")
+    }
+    let sql = "INSERT INTO pedidos SET ?";
+    await mysql.query(sql, newPedido, (err) => {
+        if(err) {
+            res.status(401).json({ err: err });
+        } else {
+            res.redirect(`/user/${req.params.usuario}`);
         }
     });
 }
--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const usuarioController = require('../controllers/usuarioController');
 
 router.get('/user/:id', usuarioController.vistaUsuario);
+router.get('/pedido/:producto/:usuario')
 
 module.exports = router;
