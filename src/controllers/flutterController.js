56ebfe1b4e5f0f63c428e20ea6082e27868eb87d
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-24 20:50:18 -0500

    etter
    ertert

--- src/controllers/flutterController.js
+++ src/controllers/flutterController.js
@@ -18,7 +18,7 @@ exports.listarProductos = async (req,res) => {
         if(err) {
             res.status(401).json({ err: err });
         } else {
-            res.status(200).json({ data: resultado[0] });
+            res.status(200).json({ data: resultado });
         }
     });
 }
