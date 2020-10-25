5d8253edef48700c47677a1d562353a51df975a8
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-24 19:52:13 -0500

    wfwe
    wefwefwe

--- src/controllers/flutterController.js
+++ src/controllers/flutterController.js
@@ -2,12 +2,12 @@ const mysql = require('../database');
 
 exports.login = async (req,res) => {
     const { email, password } = req.body;
-    let sql = "SELECT id_rol FROM usuarios WHERE correo = ? AND password = ?";
+    let sql = "SELECT * FROM usuarios WHERE correo = ? AND password = ?";
     await mysql.query(sql, [email, password], (err, resultado) => {
         if(err) {
             res.status(401).json({ err: err });
         } else {
-            res.status(200).json({ rol: resultado[0].id_rol });
+            console.log(resultado[0])
         }
     });
 }
