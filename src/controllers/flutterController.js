a3957b7ecbca22dc28109a58d79917b71765daf4
Yojhan-Alexis <54258838+Yojhan-Alexis@users.noreply.github.com>
2020-10-25 20:57:07 -0500

    SERVIDOR TERMINADO
    JHONFELIPE ES GAY Y ALEX ES UN MARICON QUE ME QUIERE QUITAR EL ADMIN PORQUE SOY UN PICHE ABUSADOR QUE INTENTA ENGAÑAR A SU AMIGO BLANQUITO QUE LE DA CONFIANZA

--- src/controllers/flutterController.js
+++ src/controllers/flutterController.js
@@ -7,7 +7,6 @@ exports.login = async (req,res) => {
         if(err) {
             res.status(401).json({ err: err });
         } else {
-            print(resultado[0]);
             res.status(200).json({ data: resultado[0] });
         }
     });
@@ -19,8 +18,23 @@ exports.listarProductos = async (req,res) => {
         if(err) {
             res.status(401).json({ err: err });
         } else {
-            print(resultado[0])
             res.status(200).json({ data: resultado[0] });
+        }
+    });
+}
+
+exports.realizarPedido = async (req,res) => {
+    let newPedido = {
+        id_usuario: req.params.usuario,
+        id_producto: req.params.producto,
+        fecha_pedido: moment(Date.now()).format("YYYY-MM-DD")
+    }
+    let sql = "INSERT INTO pedidos SET ?";
+    await mysql.query(sql, newPedido, (err) => {
+        if(err) {
+            res.status(401).json({ err: err });
+        } else {
+            res.status(200).json({ msg: "Se ha realizado el pedido" });
         }
     });
 }
--- src/routes/flutter.js
+++ src/routes/flutter.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const flutterController = require('../controllers/flutterController');
 
 router.post('/api/login', flutterController.login);
-router.get('/api/producto', flutterController.listarProductos)
+router.get('/api/producto', flutterController.listarProductos);
+router.post('/api/pedido/:producto/:usuario', flutterController.realizarPedido);
 
 module.exports = router;
